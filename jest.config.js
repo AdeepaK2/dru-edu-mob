fd6d4b0760533a6cbb9491a6b278c67fd7d52504
AdeepaK2 <adeepashashintha@gmail.com>
2025-12-26 13:48:56 +0530

    feat: add Jest setup for React Native Testing Library including mocks for various Expo and React Native modules.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: [
     '@testing-library/jest-native/extend-expect',
-    '<rootDir>/__tests__/setup.js',
+    '<rootDir>/__tests__/setup.tsx',
   ],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)',
